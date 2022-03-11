8998e04554e31898a75d729d1f09cad1ca9d7ba2
vikzak <victor@azbukait.ru>
2022-03-11 19:56:15 +0300

    ДЗ№2-пытаюсь сохранять и извлекать тему и применять - пока  не выходит

--- app/src/main/java/ru/gb/cousrematerialdesign/view/MainActivity.kt
+++ app/src/main/java/ru/gb/cousrematerialdesign/view/MainActivity.kt
@@ -2,14 +2,29 @@ package ru.gb.cousrematerialdesign.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import ru.gb.cousrematerialdesign.R
+import ru.gb.cousrematerialdesign.databinding.ActivityMainBinding
+import ru.gb.cousrematerialdesign.databinding.BottomSheetLayoutBinding
 import ru.gb.cousrematerialdesign.view.main.PictureOfTheDayFragment
 
-class MainActivity : AppCompatActivity() {
+const val ThemeGreen = 1
+const val ThemeOrange = 2
+const val ThemeIndigo = 3
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityMainBinding
+    private val KEY_SP = "sp"
+    private val KEY_CURRENT_THEME = "current_theme"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.MyBaseTheme)
-        setContentView(R.layout.activity_main)
+
+        setTheme(getRealStyle(getCurrentTheme()))
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (savedInstanceState == null){
             supportFragmentManager
                 .beginTransaction()
@@ -17,4 +32,36 @@ class MainActivity : AppCompatActivity() {
                 .commit()
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container,PictureOfTheDayFragment.newInstance())
+            .commit()
+    }
+
+    fun setCurrentTheme(currentTheme: Int){
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putInt(KEY_CURRENT_THEME,currentTheme)
+        editor.apply()
+    }
+
+    fun getCurrentTheme(): Int {
+        val sharedPreferences = getSharedPreferences(KEY_SP, MODE_PRIVATE)
+        return sharedPreferences.getInt(KEY_CURRENT_THEME,-1)
+    }
+
+    private fun getRealStyle(currentTheme: Int):Int{
+        return when(currentTheme){
+            ThemeGreen -> R.style.MyGreen
+            ThemeIndigo -> R.style.MyIndigo
+            ThemeOrange -> R.style.MyOrange
+            else -> 0
+        }
+    }
+//    private fun init() {
+//
+//    }
 }
--- app/src/main/java/ru/gb/cousrematerialdesign/view/chips/ChipsFragment.kt
+++ app/src/main/java/ru/gb/cousrematerialdesign/view/chips/ChipsFragment.kt
@@ -1,11 +1,13 @@
 package ru.gb.cousrematerialdesign.view.chips
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -19,10 +21,13 @@ import ru.gb.cousrematerialdesign.R
 import ru.gb.cousrematerialdesign.databinding.FragmentChipsBinding
 import ru.gb.cousrematerialdesign.databinding.FragmentMainBinding
 import ru.gb.cousrematerialdesign.view.MainActivity
+import ru.gb.cousrematerialdesign.view.ThemeGreen
+import ru.gb.cousrematerialdesign.view.ThemeIndigo
+import ru.gb.cousrematerialdesign.view.ThemeOrange
 import ru.gb.cousrematerialdesign.viewmodel.PictureOfTheDayDataState
 import ru.gb.cousrematerialdesign.viewmodel.PictureOfTheDayViewModel
 
-class ChipsFragment : Fragment() {
+class ChipsFragment : Fragment(), View.OnClickListener {
     private var _binding: FragmentChipsBinding? = null
     private val binding: FragmentChipsBinding
         get() = _binding!!
@@ -31,6 +36,12 @@ class ChipsFragment : Fragment() {
         fun newInstance() = ChipsFragment()
     }
 
+    private lateinit var parrentActivity:MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        parrentActivity = (context as MainActivity)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -49,9 +60,58 @@ class ChipsFragment : Fragment() {
                     .show()
             }
         }
+        binding.selectBnW.setOnClickListener {
+            if (binding.selectBnW.isChecked == true){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+
+
+
+//        when (parrentActivity.getCurrentTheme()){
+////            1 -> binding.chipGroup.findViewById<Chip>.isChecked = true
+//
+//        }
+
 
     }
 
+
+    override fun onClick(p0: View?) {
+        binding.chipGroupSelectTheme.setOnCheckedChangeListener { group, checkedId ->
+            binding.chipGroupSelectTheme.findViewById<Chip>(checkedId)?.let {
+                if (checkedId == 1){
+                    parrentActivity.setCurrentTheme(ThemeGreen)
+                    parrentActivity.recreate()
+                }
+                if (checkedId == 2){
+                    parrentActivity.setCurrentTheme(ThemeOrange)
+                    parrentActivity.recreate()
+                }
+                if (checkedId == 3){
+                    parrentActivity.setCurrentTheme(ThemeIndigo)
+                    parrentActivity.recreate()
+                }
+//                when(checkedId){
+//                    1-> {
+//                        parrentActivity.setCurrentTheme(ThemeGreen)
+//                        parrentActivity.recreate()
+//                    }
+//                    2-> {
+//                        parrentActivity.setCurrentTheme(ThemeOrange)
+//                        parrentActivity.recreate()
+//                    }
+//                    3-> {
+//                        parrentActivity.setCurrentTheme(ThemeIndigo)
+//                        parrentActivity.recreate()
+//                    }
+//                }
+            }
+        }
+
+    }
 
     override fun onDestroy() {
         super.onDestroy()
