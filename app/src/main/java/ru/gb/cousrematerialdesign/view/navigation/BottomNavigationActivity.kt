68127e73e91dd85a745e75968f26df065151c941
vikzak <victor@azbukait.ru>
2022-03-16 23:46:54 +0300

    ДЗ№3: добавлены бейджи в BottomNavigationView. По клику удаляем.

--- app/src/main/java/ru/gb/cousrematerialdesign/view/navigation/BottomNavigationActivity.kt
+++ app/src/main/java/ru/gb/cousrematerialdesign/view/navigation/BottomNavigationActivity.kt
@@ -3,6 +3,7 @@ package ru.gb.cousrematerialdesign.view.navigation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.Fragment
 import ru.gb.cousrematerialdesign.R
 import ru.gb.cousrematerialdesign.databinding.ActivityBottomNavigationBinding
 import ru.gb.cousrematerialdesign.databinding.ActivityMainBinding
@@ -19,15 +20,45 @@ class BottomNavigationActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.bottomNavigationView.setOnItemSelectedListener{
-            when(it.itemId){
-                R.id.bottom_view_earth ->{true}
-                R.id.bottom_view_mars ->{true}
-                R.id.bottom_view_system ->{true}
+        initBottomNavigationView()
+
+
+    }
+
+    private fun initBottomNavigationView() {
+        val badgeEarth = binding.bottomNavigationView.getOrCreateBadge(R.id.bottom_view_earth)
+        val badgeMars = binding.bottomNavigationView.getOrCreateBadge(R.id.bottom_view_mars)
+        val badgeSystem = binding.bottomNavigationView.getOrCreateBadge(R.id.bottom_view_system)
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_view_earth -> {
+                    binding.bottomNavigationView.removeBadge(R.id.bottom_view_earth)
+                    navigationTo(EarthFragment())
+                    true
+                }
+                R.id.bottom_view_mars -> {
+                    binding.bottomNavigationView.removeBadge(R.id.bottom_view_mars)
+                    navigationTo(MarsFragment())
+                    true
+                }
+                R.id.bottom_view_system -> {
+                    binding.bottomNavigationView.removeBadge(R.id.bottom_view_system)
+                    navigationTo(SystemFragment())
+                    true
+                }
                 else -> false
             }
         }
+        binding.bottomNavigationView.selectedItemId = R.id.bottom_view_earth
+        badgeEarth.number = 10
+        badgeMars.number = 11
+        badgeSystem.number = 77
+    }
 
-
+    private fun navigationTo(f: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.container, f)
+            .commit()
     }
 }
